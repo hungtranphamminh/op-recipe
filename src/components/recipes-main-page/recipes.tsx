e5c264e71a689b240665b5ec594f8d0c1d57bcac
artemis_h <tranphamminhhung65@gmail.com>
2024-04-15 17:25:58 +0700

    feat: update content for recipes section of the recipe page

--- src/components/recipes-main-page/recipes.tsx
+++ src/components/recipes-main-page/recipes.tsx
@@ -2,6 +2,7 @@
 import { useRecipeSearchParams } from "@/utils/store/search-store";
 import { useState, useEffect, useCallback } from "react";
 import { MOCKUP_RECIPES } from "@/utils/consts/recipe.const";
+import Image from "next/image";
 
 interface Recipe {
   title: string;
@@ -24,5 +25,24 @@ export default function Recipes() {
     setRecipes(MOCKUP_RECIPES);
   }, [name, tags, ingredients, collections]);
 
-  return <div className="w-full flex flex-wrap items-center gap-4"></div>;
+  return (
+    <div className="w-full flex flex-wrap items-center gap-4">
+      {recipes?.map((recipe, index) => (
+        <div
+          key={index}
+          className="w-[300px] flex flex-col items-center justify-center relative"
+        >
+          <div className="w-[300px]">
+            <Image
+              src={recipe.imgUrl}
+              alt={recipe.title}
+              width={400}
+              height={400}
+              className="w-full h-[400px] object-cover "
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
