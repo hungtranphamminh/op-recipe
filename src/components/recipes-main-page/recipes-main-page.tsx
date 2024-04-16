a9be4515a824c371554fb69bdabd787b14d6af03
artemis_h <tranphamminhhung65@gmail.com>
2024-04-16 14:05:54 +0700

    feat: add banner for the recipes page

--- src/components/recipes-main-page/recipes-main-page.tsx
+++ src/components/recipes-main-page/recipes-main-page.tsx
@@ -2,13 +2,25 @@
 import SingleRecipesSection from "./single-recipes";
 import { useState } from "react";
 
-const SECTIONS = ["Single Recipes", "Collections", "Ingredients"];
+const SECTIONS = ["Recommended", "Single Recipes", "Collections"];
 
 export default function RecipesMainPage() {
   const [section, setSection] = useState<number>(0);
   return (
     <div className="w-full relative py-10 flex flex-col items-center">
-      <div className="w-full relative flex flex-col items-center mt-[200px]">
+      <div className="w-full fixed -top-20 left-0 h-[100vh] bg-[url('/images/background/recipes-bg.jpg')] bg-cover bg-no-repeat bg-center"></div>
+      <div className="w-full relative z-20 h-[800px] flex flex-col items-center justify-center">
+        {/* title */}
+        <div className="font-raleWay text-6xl text-white tracking-widest">
+          DELICACIES FOR EVERYONE
+        </div>
+        <div className="w-40 h-[2px] bg-orange-300 my-6"></div>
+        <div className="font-alexBrush text-5xl text-[rgb(184,187,190)]">
+          Recipes & Collections
+        </div>
+      </div>
+
+      <div className="w-full relative flex flex-col items-center -mt-[100px] py-[100px] bg-white">
         {/* section controller */}
         <div className="w-full flex items-center justify-center">
           {SECTIONS.map((sec, index) => {
@@ -28,7 +40,7 @@ export default function RecipesMainPage() {
                 <div
                   className={`absolute -bottom-[2px] rounded-lg h-[4px] left-0
                   ${section === index ? " bg-orange-200 w-[170px]" : "w-0"}
-                  group-hover:w-[170px] transition-all duration-300 group-hover:bg-orange-200
+                  group-hover:w-[170px] transition-all duration-500 group-hover:bg-orange-200
                   `}
                 ></div>
               </button>
@@ -36,7 +48,7 @@ export default function RecipesMainPage() {
           })}
         </div>
         {/* section content */}
-        {section === 0 && <SingleRecipesSection />}
+        {section === 1 && <SingleRecipesSection />}
       </div>
     </div>
   );
