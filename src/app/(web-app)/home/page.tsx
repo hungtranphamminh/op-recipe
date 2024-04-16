7e2b25954a6cc98d76d3a99c6f9d13425e91fdd7
artemis_h <tranphamminhhung65@gmail.com>
2024-04-16 11:34:53 +0700

    feat: update recipes UI

---
+++ src/app/(web-app)/home/page.tsx
@@ -0,0 +1,13 @@
+import { LandingPage } from "@/components/landing-page/landing-page";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+export default function Home() {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  );
+}
