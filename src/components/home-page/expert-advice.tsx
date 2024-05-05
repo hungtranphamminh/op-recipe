0a7c701e1c1337c1dd92942ae3012622a627c6f3
artemis_h <tranphamminhhung65@gmail.com>
2024-05-05 19:37:23 +0700

    feat: add expert advices section

--- src/components/home-page/expert-advice.tsx
+++ src/components/home-page/expert-advice.tsx
@@ -1,37 +1,9 @@
 import ExpertAdviceDefaultCard from "../recipe-card/expert-advice-card";
-
-const MOCKUP_EA_POSTS = [
-  {
-    postName: "Pickled Red Onions Are the Quick Trick That Brighten Any Meal",
-    description:
-      "Add a pop of color and acidic punch to tacos, salads, sandwiches, and more.",
-    imgUrl:
-      "https://res.cloudinary.com/dyeiygws0/image/upload/v1709633680/Pickled-Red-Onions_HERO_ch0b6d.webp",
-  },
-  {
-    postName: "How to Make a Dish Less Spicy",
-    description: "Tame the flame in that fiery curry like a pro.",
-    imgUrl:
-      "https://res.cloudinary.com/dyeiygws0/image/upload/v1709633680/Peppers_HERO_gccuhy.webp",
-  },
-  {
-    postName: "What’s the Difference Between Lox and Smoked Salmon?",
-    description:
-      "Here's how to tell them apart—and what to know about gravlax and kippered salmon too.",
-    imgUrl:
-      "https://res.cloudinary.com/dyeiygws0/image/upload/v1709633681/What-is-Lox_HERO_bcsmo1.webp",
-  },
-  {
-    postName: "Does Vinegar Last Forever?",
-    description: "And does it matter where you store it?",
-    imgUrl:
-      "https://res.cloudinary.com/dyeiygws0/image/upload/v1709633680/Shelf-Life-of-Vinegar_HERO_r6a00u.webp",
-  },
-];
+import { MOCKUP_EA_POSTS } from "@/utils/consts/home/expert-advice-posts";
 
 const ExpertAdvice = () => {
   return (
-    <div className="mt-14 max-w-[1600px] px-16">
+    <div className="mt-10 max-w-[1600px] px-16">
       {/* title */}
       <div className="w-full flex flex-col items-center justify-start pt-8">
         <p className="text-[24px] leading-[26px] font-medium text-[rgb(51,51,51)]">
